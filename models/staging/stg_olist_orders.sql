2400b5bfe590dab44c0b2a8d0f96a1bf7ce8eb66
Zachariah Gakuya <zack@Zachariahs-MacBook-Pro.local>
2026-02-17 16:54:45 +1100

    added staging model for orders

---
+++ models/staging/stg_olist_orders.sql
@@ -0,0 +1,14 @@
+with raw_orders as (
+    select * from {{ ref('olist_orders_dataset') }}
+)
+
+select
+    order_id,
+    customer_id,
+    order_status,
+    order_purchase_timestamp as purchased_at,
+    order_approved_at as approved_at,
+    order_delivered_carrier_date as delivered_to_carrier_at,
+    order_delivered_customer_date as delivered_to_customer_at,
+    order_estimated_delivery_date as estimated_delivery_at
+from raw_orders
